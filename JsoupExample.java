5f86111e759fe7cd8da99eb275626b63a12ca62e
marianekk578@gmail.com <Xdmarian.578>
2019-09-13 13:59:58 +0200

    initial project version

---
+++ JsoupExample.java
@@ -0,0 +1,50 @@
+package APp.otomoto;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class JsoupExample {
+
+
+    public static void main(String[] args) {
+
+        long start = System.currentTimeMillis();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+
+        for (int i = 0; i < 4; i++) {
+            executorService.submit(() -> showSkarpetki());
+        }
+        executorService.shutdown();
+
+        long end = System.currentTimeMillis();
+
+        System.out.println(end - start);
+    }
+
+
+    public static void showSkarpetki() {
+
+        Document doc;
+        String url = "https://www.soxinthebox.pl/";
+
+        try {
+            doc = Jsoup.connect(url).get();
+            Elements img = doc.getElementsByTag("img");
+            for (Element element : img) {
+                String src = element.absUrl("src");
+                if (src.contains("skarpet"))
+                    System.out.println(src);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
